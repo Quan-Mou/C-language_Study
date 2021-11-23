49bdf0df027e0812c266d8e180d76206681cabeb
Root_QuanMou <2312038309@qq.com>
2021-11-23 22:57:48 +0800

    整型在内存中的存储

---
+++ test_11_21/test_11_21/test.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	//数据在内存中的存储
+	//1.数据类型详细介绍
+	//2. 整形在内存中的存储：原码、反码、补码
+	//3. 大小端字节序介绍及判断
+	
+	//数据类型介绍
+	char //字符数据类型
+		int //整型数据类型
+		short // 短整型
+		long // 长整型
+		long long // 更长的整型
+		float // 单精度浮点型
+		double // 双精度浮点型
+
+		//类型的意义：合理使用内存，这个类型的数据决定了使用范围
+
+		//类型的基本归类：
+		//整型家族：
+		//char 
+		//	unsigned char 
+		//	signed char
+		//int 
+		//	unsigned int
+		//	signed int
+
+		//short
+		//	unsigned short
+		//	signed short
+		//long
+		//	unsigned long
+		//	signed long
+
+		//浮点型家族：
+		//float
+		//double
+
+		//构造类型：
+		//数据类型
+		//结构体类型
+		//枚举类型
+		//联合类型
+
+		//指针类型：
+		//int*
+		//char*
+		//float*
+		//double*
+
+		//空类型：
+		//void表示空类型（无类型）比如：函数的返回值，函数传参 指针类型
+
+		//整型在内存中的存储
+		int a = 10;
+	int b = -20;
+
+
+	// 看比特科技
+
+    return 0;
+}
